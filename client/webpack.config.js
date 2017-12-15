8256488393cbe2a5141e633a5e4a517474f20578
santhoshRaju91 <santhosh.codeholic@gmail.com>
2017-12-15 17:44:32 +0530

    Client react application setup

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -1,22 +1,58 @@
 var path = require('path');
+var HtmlWebpackPlugin = require('html-webpack-plugin');
+var ExtractTextPlugin = require('extract-text-webpack-plugin');
+
+
+const extractSass = new ExtractTextPlugin({
+      filename: "[name].[contenthash].css",
+      disable: process.env.NODE_ENV === "development"
+  });
+
 var config = {
-   entry: './src/App.jsx', // entry point
-   output: {
-         path:path.join(__dirname, 'public'),
-         filename: 'bundle.js', // place where bundled app will be served
+      entry: './src/App.jsx',
+      output: {
+            path: path.join(__dirname, 'dist'),
+            filename: 'bundle.js',
       },
-   devServer: {
-         port: 5000, // development port server
-         publicPath:"/public/"
+      devServer: {
+            port: 5000, 
+            publicPath: "/dist/"
       },
-   module: {
-         loaders: [
-            {
-               test: /\.jsx?$/, // search for js files
-               exclude: /node_modules/,
-               loader: 'babel-loader',
-         }
+      module: {
+            loaders: [
+                  {
+                        test: /\.jsx?$/,
+                        loader: 'babel-loader',
+                        exclude: /node_modules/                        
+                  },
+                  {
+                        test: /\.js?$/,
+                        loader: 'babel-loader',
+                        exclude: /node_modules/
+                  },
+                  {
+                        test: /\.scss?$/,
+                        use: ExtractTextPlugin.extract({
+                              use: ['css-loader', 'sass-loader'],
+                              fallback: 'style-loader'
+                        })
+                  },
+                  {
+                        test: /\.(jpe?g|gif|png|eot|svg|woff|woff2|ttf)$/,
+                        use: 'file-loader'
+                  }
+            ]
+      },
+
+      plugins: [
+            new HtmlWebpackPlugin({
+                  template: './index.html',
+                  filename: 'index.html',
+                  inject: 'body'
+            }),
+
+            extractSass
       ]
-   }
-}
+};
+
 module.exports = config;
