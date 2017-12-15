b067e56372d446fc831ffdb83ed1932964ea1ecf
santhoshRaju91 <santhosh.codeholic@gmail.com>
2017-12-15 20:36:14 +0530

    Adding supported and unsupported viewss

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -6,7 +6,7 @@ export default class App extends Component {
         return (
             <div>
                 <div className="container-fluid">
-                    <h3 className="text-center">Checking for render with hot reloading!!</h3>  
+                    <h3 className="text-center">Hot module reloading works</h3>  
                     <img src={Show} alt="Show image"/>
                 </div>
             </div>
